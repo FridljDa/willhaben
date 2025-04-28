53284cb5df536fca08c209f00d3a1732ea7e70ad
danielfrid <daniel.fridljand@tngtech.com>
2025-04-28 12:47:05 +0200

    update README and wien_query.py to reflect Python usage and new listing retrieval method

--- playground/wien_query.py
+++ playground/wien_query.py
@@ -1,8 +1,9 @@
-import willhaben
+from app import get_listings
+
 
 url_query = 'https://www.willhaben.at/iad/immobilien/mietwohnungen/mietwohnung-angebote?sort=1&rows=30&isNavigation=true&sfId=7c07d0eb-68b5-46e8-8e40-e47a743a85b0&ESTATE_PREFERENCE=28&areaId=117223&areaId=117224&areaId=117225&areaId=117226&areaId=117227&areaId=117228&areaId=117229&areaId=117230&areaId=117231&page=1&PRICE_FROM=0&PRICE_TO=1100&ESTATE_SIZE/LIVING_AREA_FROM=40'
 
-listings = willhaben.get_listings(url_query)
+listings = get_listings(url_query)
 #location,
 #                 postcode,
 #                 description,
@@ -21,4 +22,4 @@ filtered_listings = [
 ]
 
 # Uncomment the following line to print the filtered listings
-# print(filtered_listings)
+print(filtered_listings)
