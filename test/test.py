933cc9aaf34dbbfb15f240146237c2fb6d13b63c
Daniel Fridljand <daniel.fridljand@gmail.com>
2025-05-11 21:03:51 +0200

    refactor test.py to adjust import statements for improved module accessibility

--- test/test.py
+++ test/test.py
@@ -1,6 +1,11 @@
-import unittest
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from src.listings_overview_fetcher import ListingsOverviewFetcher
 from src.multiple_listings import MultipleListings
+
+import unittest
 
 
 class TestWillhabenAPI(unittest.TestCase):
