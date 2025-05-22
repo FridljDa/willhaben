7aa697dbd3288c27e11342afeedf5ba56ac8bde2
danielfrid <daniel.fridljand@tngtech.com>
2025-05-22 22:35:51 +0200

    refactor: enhance ListingDetailsFetcher and introduce listing_fetch_script for streamlined listing processing

--- main.py
+++ main.py
@@ -1,21 +1,6 @@
-from listing.fetcher.listing_details_fetcher import ListingDetailsFetcher
-from listing.fetcher.listings_overview_fetcher import ListingsOverviewFetcher
-from listing.structure.multiple_listings import MultipleListings
-from relevant_columns import relevant_keys
+from listing.listing_fetch_script import fetch_and_process_listings
 
 if __name__ == "__main__":
-    multiple_listings = MultipleListings()
-
     url_query = 'https://www.willhaben.at/iad/immobilien/mietwohnungen/mietwohnung-angebote?sort=1&rows=1000&isNavigation=true&sfId=7c07d0eb-68b5-46e8-8e40-e47a743a85b0&ESTATE_PREFERENCE=28&areaId=117223&areaId=117224&areaId=117225&areaId=117226&areaId=117227&areaId=117228&areaId=117229&areaId=117230&areaId=117231&page=1&PRICE_FROM=0&PRICE_TO=1100&ESTATE_SIZE/LIVING_AREA_FROM=40'
-    ListingsOverviewFetcher(url_query, multiple_listings).fetch_all_listings()
-    multiple_listings.apply_function_to_each_listing(
-        ListingDetailsFetcher.fetch_and_set_single_listing_content
-    )
 
-    multiple_listings.subselect_listing_keys_and(relevant_keys)
-
-    multiple_listings.write_multiple_listings_to_txt_json_file()
-    multiple_listings.write_multiple_listings_to_csv_file()
-
-    multiple_listings.pretty_print()
-    print(f"Number of listings: {len(multiple_listings.list_of_listings)}")
+    fetch_and_process_listings(url_query)
--- src/listing/fetcher/listing_details_fetcher.py
+++ src/listing/fetcher/listing_details_fetcher.py
@@ -1,6 +1,7 @@
 import logging
 
 from listing.fetcher.fetcher import Fetcher
+from listing.structure import single_listing
 from listing.structure.single_listing import SingleListing
 
 # Configure logging
@@ -12,21 +13,20 @@ class ListingDetailsFetcher(Fetcher):
     Fetches and processes details for a single listing.
     """
 
-    def __init__(self, url: str):
+    def __init__(self, single_listing_with_url: SingleListing):
         """
         Initializes the fetcher with a URL.
         """
-        super().__init__(url)  # Initialize the parent class
+        super().__init__(single_listing_with_url.listing_data['url'])  # Initialize the parent class
+        self.single_listing = single_listing_with_url
 
-    @staticmethod
-    def fetch_and_set_single_listing_content(single_listing: SingleListing) -> None:
+    def fetch_and_set_single_listing_content(self) -> None:
         """
         Extracts and sets the listing details from the soup.
         """
-        url = single_listing.listing_data['url']
-        parsed_data = Fetcher.fetch_content_as_json(url)  # Use inherited method
+        parsed_data = Fetcher.fetch_content_as_json(self.url)  # Use inherited method
 
         listing_attribute = parsed_data['props']['pageProps']['advertDetails']['attributes']['attribute']
         for listing_attribute_key_value in listing_attribute:
-            single_listing.listing_data[listing_attribute_key_value["name"]] = listing_attribute_key_value["values"]
+            self.single_listing.listing_data[listing_attribute_key_value["name"]] = listing_attribute_key_value["values"]
 
